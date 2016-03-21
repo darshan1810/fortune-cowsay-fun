4f4bfdac417debcbc48dbf934af24bb772f652f2
Darshan Godhia <darshan1810@gmail.com>
2016-03-21 16:47:33 +0530

    force yes - install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@
 ############################################
 
 # Fortune
-sudo apt-get install fortune
+sudo apt-get install fortune -y --force-yes
 
 # Cowsay
-sudo apt-get install cowsay
+sudo apt-get install cowsay -y --force-yes
+

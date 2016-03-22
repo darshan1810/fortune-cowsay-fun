e9aba9ad7dc094cef6b3f59712b255a72a234a20
verbal27 <smiletobehappy27@gmail.com>
2016-03-23 01:08:55 +0530

    Added basic OS specific installation

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+if grep -q "Ubuntu" "/proc/version"; then
+	############################################
+	#          Ubuntu installation             #
+	############################################
 
-############################################
-#          Ubuntu installation             #
-############################################
+	# Fortune
+	sudo apt-get install fortune -y --force-yes
 
-# Fortune
-sudo apt-get install fortune -y --force-yes
+	# Cowsay
+	sudo apt-get install cowsay -y --force-yes
+exit 0
+fi
 
-# Cowsay
-sudo apt-get install cowsay -y --force-yes
+if grep -q "Red Hat" "/proc/version"; then
+	############################################
+	#	  Red Hat installation		   #
+	############################################
 
+	# Fortune
+	yes | sudo yum install fortune
+
+	# Cowsay
+	yes | sudo yum install cowsay
+exit 0
+fi
